0c9e61e0734060adffef6d213ad76bef172e31f2
Avikant Srivastava <aesthetixell@pop-os.localdomain>
2021-09-05 04:09:56 +0530

    added login and signup

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from fastapi import FastAPI
+from database.database import Database
+from helper.hashing import hash
+from helper.auth import AuthHandler
+from models.user import User 
+
+app = FastAPI()
+user_db = Database(os.environ.get('DETA'), 'users')
+auth_handler = AuthHandler() 
+
+
+
+@app.post("/signup")
+async def signup(user : User):
+    user_id = hash(user.username, 'secret')
+    if not user_db.fetch(user_id):
+        user.password_hash = auth_handler.get_password_hash(user.password)
+        user_db.add(
+            user_id,
+            user.dict(exclude = {'password'})
+        )
+        
+        return {'status': 'success'}
+        
+    else :
+        return {'status': 'already exists'}
+    
+    
+@app.post("/login")
+async def login(user : User):
+    user_id = hash(user.username, 'secret')
+    
+    
+    try:
+        user_from_db = user_db.fetch(user_id)
+        user_from_db['value']['id'] = user_id
+        print(user_from_db)
+
+        
+        if auth_handler.verify_password(user.password, user_from_db['value']['password_hash']):
+            return {'status': 'success'}
+        return {'status': 'wrong password'}
+    
+    except :
+        return {'status': 'user not found'}
